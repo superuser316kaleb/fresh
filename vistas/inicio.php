f3cd78a4bb022bc404a1875a861c7cac017da2b7
dianaoccen2503 <21031023@itcelaya.edu.mx>
2024-05-23 18:27:44 -0600

    prueba

--- vistas/inicio.php
+++ vistas/inicio.php
@@ -3,12 +3,22 @@
       <link rel="stylesheet" href="../CSS/styles.css">
     </header>
     <body>
+      
 <div class="has-background-danger-85 is-fullwidth" >
+  <h1 class="cssFont_1" >FRESH</h1>
     <div class="container-is-full has-background-primary-90">
         <div class="columns is-centered">
             <div class="column is-full">
-                <h1 class="text-white" >FRESH</h1>
-                <p class="subtitle is-4 has-text-centered has-text-link">Simulador de VENDING MACHINE</p>
+                <p class="subtitle is-4 has-text-centered has-text-link">Elige un sabor</p>
+                <button >
+                  <img src="..\imgs\images.jpeg" width="200px" height="130">
+                </button>
+              <button>
+                <img src="..\imgs\h.jpg" width="200px" height="130">
+              </button>
+            <button >
+              <img src="..\imgs\limon.jpeg" width="200px" height="130">
+            </button>
             </div>
         </div>
     </div>
@@ -17,8 +27,8 @@
   </div>
   <div class="hero-body is-fullwidth">
     <div class="container has-text-centered">
-      <p class="title">Aquí ira una imagen default de bebidas</p>
-      <p class="subtitle">hola</p>
+      <p class="title">ELIGE UN TAMAÑO DE VASO</p>
+      <p class="subtitle">(chico,mediano y grande)</p>
     </div>
     <section class="section is-medium">
   <h1 class="title has-text-centered">SECION ESCALABLE</h1>
